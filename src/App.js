a56474e1b9939d566cdd32c18b6d78bc7f496086
unknown <ilhamriana3@gmail.com>
2019-02-22 21:24:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+
+function Home() {
+  return <h2> Halaman Baru </h2>
+}
+
+function ListView() {
+  return (
+    <div>
+    <h2> Semua User </h2>
+    <ul>
+        <Link to='user/ilham'>Ilham</Link>
+        <br />
+        <Link to='user/ucok'>Ucok</Link>
+    </ul>
+    </div>
+  )
+}
+
+function DetailView({ match }) {
+  return <h2> Ini Halaman {match.params.name} </h2>
+}
+
+function NoMatch() {
+  return <h2> 404, halaman tidak ditemukan </h2>
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+          <div>
+              <nav>
+                  <li> <Link to='/'> Home </Link> </li>
+                  <li> <Link to='/users'> User </Link> </li>
+              </nav>
+
+              <main>
+                <Switch>
+                  <Route path='/' exact component={Home} />
+                  <Route path='/users' exact component={ListView} />
+                  <Route path='/user/:name' exact component={DetailView} />
+                  <Route component={NoMatch} />
+                </Switch>
+              </main>
+          </div>
+      </BrowserRouter>
     );
   }
 }
